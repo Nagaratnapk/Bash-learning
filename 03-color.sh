fa390790507a9f3ca063e47d1dea50f7c1325f75
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MacBook-Pro.local>
2022-09-06 22:08:17 -0700

    changing the color

---
+++ 03-color.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Syntax : echo -e  "\e[ColorCodem Your Text \e[0m"
+
+# Syntax is 
+# echo -e "\e[32m TextOfYourChoice  \e[0m"
+
+echo -e "\e[38m DevOps has a very great future \e[0m"
+echo -e "\e[47;37m DevOps has a very great future \e[0m"
