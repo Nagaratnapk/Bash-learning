235c62e92012cdc2f6de83ead9f5eeefc3f6f76f
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MacBook-Pro.local>
2022-09-06 22:13:36 -0700

    changed

--- 03-color.sh
+++ 03-color.sh
@@ -5,4 +5,4 @@
 # echo -e "\e[32m TextOfYourChoice  \e[0m"
 
 echo -e "\e[38m DevOps has a very great future \e[0m"
-echo -e "\e[47;37m DevOps has a very great future \e[0m"
+echo -e "\e[42;37m DevOps has a very great future \e[0m"
