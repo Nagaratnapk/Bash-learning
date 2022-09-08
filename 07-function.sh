cba1ff6c26cecb4a708d02abe3f31f78ca2ef586
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MacBook-Pro.local>
2022-09-07 20:19:11 -0700

    start

--- 07-function.sh
+++ 07-function.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-sample() {
-   echo "hai from the sample function"
-   echo "I will be printing the uptime" 
- }
+
+#sample() {
+ #  echo "hai from the sample function"
+  # echo "I will be printing the uptime" 
+# }
+
+
+# Defining the function stat
+stat() {
+    echo "Load Average on the system from last 1 min is : $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
+    echo "Number of users signed in is : $(who|wc -l)" 
+    echo "Stat Function is completed bye"
+} 
