906308f493bb2ff3a1794d6e9067b85a1ab359e3
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MacBook-Pro.local>
2022-09-09 20:22:08 -0700

    update

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+set -e 
+source components/common.sh
+# source is a command to import a file and run it locally
+
+COMPONENT=mongodb
+
+echo -n "Configuring the MongoDB Repo:"
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
+stat $?
+
+echo -n "Installing ${COMPONENT}:"
+yum install -y mongodb-org >> /tmp/${COMPONENT}.log
+stat $? 
+
+# yum install -y mongodb-org
+# systemctl enable mongod
+# systemctl start mongod
