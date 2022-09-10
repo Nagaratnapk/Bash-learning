625ef2ec26661af28d55f32937225d86bb439a47
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MacBook-Pro.local>
2022-09-09 21:04:20 -0700

    update

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -6,13 +6,23 @@ source components/common.sh
 COMPONENT=mongodb
 
 echo -n "Configuring the MongoDB Repo:"
-curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
+curl -s -o /etc/yum.repos.d/{COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
 stat $?
 
 echo -n "Installing ${COMPONENT}:"
 yum install -y mongodb-org >> /tmp/${COMPONENT}.log
 stat $? 
 
-# yum install -y mongodb-org
-# systemctl enable mongod
-# systemctl start mongod
+echo -n "Updating the ${COMPONENT} config:"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
+stat $? 
+
+echo -n "Start the ${COMPONENT} service"
+systemctl enable mongod >> /tmp/${COMPONENT}.log
+systemctl start mongod  
+stat $? 
+
+echo -n "Downloading the schema:"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
+
